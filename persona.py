120efd481840bf4f0f2d6f8620d7e7a603dcaafe
Francisco <franovelar13@gmail.com>
2019-02-07 10:23:50 -0300

    se agregaron dos personas y se modifico edad

--- persona.py
+++ persona.py
@@ -7,11 +7,23 @@
 #Al instanciar la clase tiene que saludar igual que el dino diciendo sus atributos 
 
 class persona: 
-
     def __init__(self, su_nombre, su_edad, su_profesion):
         self.nombre = su_nombre
         self.edad = su_edad
         self.profesion = su_profesion
         print("Hola soy un humano, mi nombre es", self.nombre, "tengo", self.edad, "anhos", "trabajo como", self.profesion)
+    
+    def cumpleanhos(self):
+        self.edad += 1 
+        return self.edad 
 
-humano = persona("Carlos", "40", "Pombero")    
+Carlos = persona("Carlos", 40, "Pombero loco")
+Eduardo = persona("Edu", 35, "plomero de plomo") 
+Jean = persona("Mr. Jean", 65, "oficinista de dia, ninja de noche")
+
+Carlos.cumpleanhos()
+Eduardo.cumpleanhos()
+Jean.cumpleanhos()
+
+#agregar un metodo a la clase "persona" que se llame cumpleanhos y que aumente la edad de la 
+#persona en un anho y retorne la edad nueva 
